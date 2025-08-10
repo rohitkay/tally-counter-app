a35a812c34467513812a4b1418bb39b1b9231433
Rohit K <192195109+rohitkay@users.noreply.github.com>
2025-08-10 13:55:35 +1000

    Setup project intital files

---
+++ Tally Counter App/Tally Counter App/Tally_Counter_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tally_Counter_AppApp.swift
+//  Tally Counter App
+//
+//  Created by rohit on 10/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct Tally_Counter_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tally Counter App/Tally Counter AppTests/Tally_Counter_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Tally_Counter_AppTests.swift
+//  Tally Counter AppTests
+//
+//  Created by rohit on 10/8/2025.
+//
+
+import Testing
+
+struct Tally_Counter_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
